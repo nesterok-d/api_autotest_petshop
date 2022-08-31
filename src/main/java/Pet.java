115dc81299f601ea6faaaf797e53f1ab10c6a223
nesterok-d <nesr.daria.nest@gmail.com>
2022-08-31 18:34:24 +0700

    created getter and setter for pet

--- src/main/java/Pet.java
+++
@@ -1,5 +0,0 @@
-public class Pet {
-    private String name;
-    private String photoUrl;
-    private String status;
-}
---
+++ src/main/java/pages/Pet.java
@@ -0,0 +1,26 @@
+package pages;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Pet {
+    private String name;
+
+    public Pet() {
+    }
+
+    public Pet(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
