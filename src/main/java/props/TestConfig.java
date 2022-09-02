e29eefa422550b2998d428f9f48661e632f6cea1
nesterok-d <nesr.daria.nest@gmail.com>
2022-09-02 13:38:13 +0700

    change config for test case

---
+++ src/main/java/props/TestConfig.java
@@ -0,0 +1,13 @@
+package props;
+
+public enum TestConfig {
+
+    Uri("uri"),
+    Path("path");
+
+    public final String value;
+
+    TestConfig(String value) {
+        this.value = Configuration.getConfiguration().getProperty(value);
+    }
+}
